bfc386d84007fa359e2462df4de5235fc462c0d3
shida-yang <93886300+yang-shida@users.noreply.github.com>
2022-03-02 18:20:58 -0500

    remove testing code

--- Home-Cam-Frontend/src/app/camera-card/camera-card.component.ts
+++ Home-Cam-Frontend/src/app/camera-card/camera-card.component.ts
@@ -24,44 +24,4 @@ export class CameraCardComponent implements OnInit {
     );
   }
 
-  buttonClick(): void {
-    // toggle flash
-    // this.cameraServices.getCamSetting("94:b9:7e:fa:e1:28").subscribe(
-    //   camSettings => {
-    //     this.cameraServices.updateCamSetting({...camSettings, flashLightOn: !camSettings.flashLightOn}).subscribe()
-    //   }
-    // );
-
-    // list cameras
-    // this.cameraServices.getActiveCameras().subscribe(
-    //   camList => {
-    //     console.log(camList)
-    //   }
-    // )
-
-    // refresh then list
-    // this.cameraServices.refreshCameraList().subscribe(
-    //   camList => {
-    //     console.log(camList)
-    //   }
-    // )
-
-    // preview
-    // this.imageUrl = this.cameraServices.getCameraPreviewImageUrl("94:b9:7e:fa:e1:28");
-
-    // stream
-    // this.imageUrl = this.cameraServices.getStreamingUrl("94:b9:7e:fa:e1:28");
-
-    // playback
-    // this.imageUrl = this.cameraServices.getPlaybackUrl("94:b9:7e:fa:e1:28", 1645192638880);
-
-    // show time interval
-    // this.cameraServices.getAvailableRecordingTimeIntervals("94:b9:7e:fa:e1:28", 1645192638880, 1000*3600*24*14).subscribe(
-    //   intervals => {
-    //     console.log(intervals)
-    //   }
-    // )
-    
-  }
-
 }
